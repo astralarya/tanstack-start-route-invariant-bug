f97d88f01e3ea9bd525bcfd9594fb630f0fca072
Mara Kim <mara@astralarium.net>
2025-12-18 16:05:19 -0800

    setup links

--- src/routes/_pathless_layout.tsx
+++ src/routes/_pathless_layout.tsx
@@ -7,11 +7,21 @@ export const Route = createFileRoute('/_pathless_layout')({
 function RouteComponent() {
   return (
     <div>
-      <div>Hello "/_pathless_layout"!</div>
-      <div>
-        <Link to={'/'}>Link to "/"</Link>
-        <Link to={'/nested_path'}>Link to "/nested_path"</Link>
+      <div className="bg-slate-300 flex gap-2 p-1">
+        <Link
+          className="bg-slate-500 hover:bg-slate-400 rounded-sm p-1"
+          to={'/'}
+        >
+          Link to "/"
+        </Link>
+        <Link
+          className="bg-slate-500 hover:bg-slate-400 rounded-sm p-1"
+          to={'/nested_path'}
+        >
+          Link to "/nested_path"
+        </Link>
       </div>
+      <div>Hello "/_pathless_layout"!</div>
       <Outlet />
     </div>
   )
